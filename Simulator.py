cfb5c8b04bede952051356af7b70be0ec913dd17
Payal Joshi <pjoshi@pdx.edu>
2018-05-26 08:09:32 -0700

    fcfs chnages to return a list

--- Simulator.py
+++ Simulator.py
@@ -6,30 +6,35 @@ from Fcfs import Fcfs as fcfs
 from Priority import Priority as  priority
 
 
-def simulator(algorithm, num_of_jobs, cpu_slice):
-    algorithm_to_execute = algorithm.capitalize()
-    job_List = Job.create_Jobs(num_of_jobs)
+class Simulator:
 
-    if(algorithm_to_execute == "Fcfs"):
-        fcfs.execute_fcfs(num_of_jobs,cpu_slice,job_List )
+    fcfs_job_list = []
+    priority_job_list = []
+    linux_job_list = []
 
-    elif algorithm_to_execute == "Priority":
-        priority.execute_priority(num_of_jobs,cpu_slice,job_List)
+    def simulator(algorithm, num_of_jobs, cpu_slice):
+        algorithm_to_execute = algorithm.capitalize()
+        job_List = Job.create_Jobs(num_of_jobs)
 
+        if(algorithm_to_execute == "Fcfs"):
+            Simulator.fcfs_job_list = fcfs.execute_fcfs(num_of_jobs,cpu_slice,job_List )
 
-def main():
-    print("Starting Simulator")
-    cpuTimer = Timer.Timer(count=50, step=1)
-    #cpuTimer.start_Timer()
-    #cpuTimer.stop()
+        elif algorithm_to_execute == "Priority":
+            priority.execute_priority(num_of_jobs,cpu_slice,job_List)
 
-    #Create required number of Jobs for scheduling using Jobs.Creat_Jobs
-    #job_List=Job.create_Jobs(200)
-    simulator("priority", 4, 5)
+    def main():
+        print("Starting Simulator")
+        cpuTimer = Timer.Timer(count=50, step=1)
+        #cpuTimer.start_Timer()
+        #cpuTimer.stop()
+
+        #Create required number of Jobs for scheduling using Jobs.Creat_Jobs
+        #job_List=Job.create_Jobs(200)
+        Simulator.simulator("fcfs", 4, 5)
 
 
 
 if __name__== "__main__":
-  main()
+    Simulator.main()
 
 
